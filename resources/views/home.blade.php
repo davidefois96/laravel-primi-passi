0b854addbd75a6f73526dcd9396fdf38fae07483
davidefois96 <davidefois360@gmail.com>
2024-05-09 17:59:03 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
+    <title>Hello Laravel</title>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark text-white px-5 ">
+
+        <a class="text-decoration-none text-white" href="{{ route('home') }}">HOME</a>
+        <a class="text-decoration-none text-white" href="{{ route('about') }}">CHI SIAMO</a>
+    </nav>
+
+    <div class="container text-center">
+
+        <h1 class="my-5">HELLO WORLD HOME</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus recusandae nostrum obcaecati corporis. Ipsum, doloribus quas cupiditate voluptates rerum illum quidem placeat non molestiae distinctio saepe at, doloremque velit ipsa.</p>
+
+
+    </div>
+
+</body>
+</html>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
